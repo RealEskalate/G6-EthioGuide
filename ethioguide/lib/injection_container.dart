89ae3f705abc77dcdaf7430ecc4fece82edecbe2
Rajaf Dereje <rajafgudeta@gmail.com>
2025-08-28 16:24:14 +0300

    add injection_container file with basic strcture

---
+++ ethioguide/lib/injection_container.dart
@@ -0,0 +1,19 @@
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  //* Features - 
+  // Bloc
+
+  // Usecase
+
+  // Repositories
+
+  // Datasources
+
+
+  //! Core
+
+  //! External
+}
