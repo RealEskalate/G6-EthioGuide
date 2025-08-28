6fe1ae22a81e10e1c99d26112e6d0e2bb7f08d83
Rajaf Dereje <rajafgudeta@gmail.com>
2025-08-28 23:07:19 +0300

    add dio to dependency injection file

--- ethioguide/lib/injection_container.dart
+++ ethioguide/lib/injection_container.dart
@@ -1,9 +1,14 @@
+import 'package:dio/dio.dart';
+import 'package:ethioguide/core/data/repositories/auth_repository_impl.dart';
+import 'package:ethioguide/core/domain/repositories/auth_repository.dart';
+import 'package:ethioguide/core/network/interceptors/auth_interceptor.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
-  //* Features - 
+  //* Features -
   // Bloc
 
   // Usecase
@@ -12,8 +17,22 @@ Future<void> init() async {
 
   // Datasources
 
-
   //! Core
+  //* Data Layer (Repositories)
+  sl.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(secureStorage: sl()),
+  );
+
+  //* Network (Dio & Interceptors)
+  sl.registerLazySingleton(() => AuthInterceptor());
 
   //! External
+  sl.registerLazySingleton(() => const FlutterSecureStorage());
+  // todo: replace with actural base url
+  sl.registerLazySingleton<Dio>(() {
+    final dio = Dio(BaseOptions(baseUrl: 'YOUR_API_BASE_URL'));
+    dio.interceptors.add(sl<AuthInterceptor>());
+    // You might add other interceptors here too
+    return dio;
+  });
 }
