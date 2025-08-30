b74ef09ad0b34328a2fd8eb078cd2b47d01190f0
Rajaf Dereje <rajafgudeta@gmail.com>
2025-08-30 23:39:28 +0300

    add ai chat feature dependency

--- ethioguide/lib/injection_container.dart
+++ ethioguide/lib/injection_container.dart
@@ -2,6 +2,14 @@ import 'package:dio/dio.dart';
 import 'package:ethioguide/core/data/repositories/auth_repository_impl.dart';
 import 'package:ethioguide/core/domain/repositories/auth_repository.dart';
 import 'package:ethioguide/core/network/interceptors/auth_interceptor.dart';
+import 'package:ethioguide/features/AI%20chat/Domain/repository/ai_repository.dart';
+import 'package:ethioguide/features/AI%20chat/Domain/usecases/get_history.dart';
+import 'package:ethioguide/features/AI%20chat/Domain/usecases/send_query.dart';
+import 'package:ethioguide/features/AI%20chat/Domain/usecases/translate_content.dart';
+import 'package:ethioguide/features/AI%20chat/Presentation/bloc/ai_bloc.dart';
+import 'package:ethioguide/features/AI%20chat/data/datasources/ai_local_datasource.dart';
+import 'package:ethioguide/features/AI%20chat/data/datasources/ai_remote_datasource.dart';
+import 'package:ethioguide/features/AI%20chat/data/repository/ai_repository_impl.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 
@@ -16,6 +24,41 @@ Future<void> init() async {
   // Repositories
 
   // Datasources
+
+  
+  //* Features - Ai chat
+  // Bloc
+  sl.registerFactory<AiBloc>(
+    () => AiBloc(
+      sendQueryUseCase: sl(),
+      getHistoryUseCase: sl(),
+      translateContentUseCase: sl(),
+    ),
+  );
+
+  // Usecase
+  sl.registerLazySingleton<SendQuery>(() => SendQuery(repository: sl()));
+  sl.registerLazySingleton<GetHistory>(() => GetHistory(repository: sl()));
+  sl.registerLazySingleton<TranslateContent>(
+    () => TranslateContent(repository: sl()),
+  );
+
+  // Repositories
+  sl.registerLazySingleton<AiRepository>(
+    () => AiRepositoryImpl(
+      remoteDatasource: sl(),
+      localDatasource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+
+  // Datasources
+  sl.registerLazySingleton<AiRemoteDatasource>(
+    () => AiRemoteDataSourceImpl(dio: sl(), networkInfo: sl()),
+  );
+  sl.registerLazySingleton<AiLocalDatasource>(
+    () => AiLocalDataSourceImpl(secureStorage: sl()),
+  );
 
   //! Core
   //* Data Layer (Repositories)
