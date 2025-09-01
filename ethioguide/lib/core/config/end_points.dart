af1d70a0b4b4f389e3afebe8aead98a24db09239
Rajaf Dereje <rajafgudeta@gmail.com>
2025-09-01 10:46:41 +0300

    add ai feature endpoints

--- ethioguide/lib/core/config/end_points.dart
+++ ethioguide/lib/core/config/end_points.dart
@@ -1,3 +1,7 @@
 class EndPoints {
+  static final baseUrl = '';
   static final refreshTokenEndPoint = '';
+  static final sendQueryEndPoint = '/ai/guide';
+  static final getHistoryEndPoint = '/ai/history';
+  static final translateContentEndPoint = '/translate';
 }
