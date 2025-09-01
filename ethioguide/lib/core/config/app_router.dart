675d78cbc424d8493bfc503e729b5837eac8de33
Rajaf Dereje <rajafgudeta@gmail.com>
2025-09-01 15:31:29 +0300

    ai chat page

--- ethioguide/lib/core/config/app_router.dart
+++ ethioguide/lib/core/config/app_router.dart
@@ -1,29 +1,36 @@
 import 'package:ethioguide/core/config/route_names.dart';
+import 'package:ethioguide/features/AI%20chat/Presentation/screens/ai_chat_screen.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ethioguide/features/splashscreen/presentation/screens/placeholder_screen.dart';
 import 'package:ethioguide/features/splashscreen/presentation/screens/splash_screen.dart';
 
 // This is the central router configuration for the entire application.
 final GoRouter router = GoRouter(
-
-  initialLocation: '/',
+  // TODO: return it to root path
+  initialLocation: '/aiChat',
 
   // The 'routes' list contains all the possible pages the user can navigate to.
   routes: [
     //  This is the route for the splash screen.
     GoRoute(
-      path: '/', 
-      name: RouteNames.splash, 
-      builder: (context, state) => const SplashScreen(), // The function that builds the widget for this screen.
+      path: '/',
+      name: RouteNames.splash,
+      builder: (context, state) =>
+          const SplashScreen(), // The function that builds the widget for this screen.
     ),
 
     // This is the route for our temporary placeholder screen.
     GoRoute(
-      path: '/placeholder', 
+      path: '/placeholder',
       name: 'placeholder',
       builder: (context, state) => const PlaceholderScreen(),
     ),
 
-    
+    // This is the route for AI chat page
+    GoRoute(
+      path: '/aiChat',
+      name: RouteNames.aiChat,
+      builder: (context, state) => const ChatPage(),
+    ),
   ],
 );
