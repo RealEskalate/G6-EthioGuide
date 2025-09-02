aea6e7defa890f2488a5c81674c4bf83e3dd3fbc
sosena2 <149303452+sosena2@users.noreply.github.com>
2025-09-02 17:26:11 +0300

    feat: refined the notice page

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -9,11 +9,29 @@ export default function UserLayout({
   children: React.ReactNode
 }) {
   return (
-      <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <div className="flex flex-1">
-        <Sidebar />
-  <main className="flex-1 p-4 md:p-8">{children}</main>
+      <div className="flex flex-1 flex-col md:flex-row">
+        {/* Sidebar: hidden on mobile, visible on md+ screens */}
+        <aside className="hidden md:flex md:flex-col md:w-64 flex-shrink-0 h-full">
+          <div className="flex-1">
+            <Sidebar />
+          </div>
+          <div className="p-4">
+            {/* Place your Logout button/component here */}
+            {/* Example: */}
+            {/* <Button className="w-full bg-red-600 text-white">Logout</Button> */}
+          </div>
+        </aside>
+        {/* Mobile sidebar: visible only on mobile, collapsible */}
+        <aside className="md:hidden w-full flex flex-col">
+          <Sidebar />
+          <div className="p-4">
+            {/* Place your Logout button/component here */}
+            {/* <Button className="w-full bg-red-600 text-white">Logout</Button> */}
+          </div>
+        </aside>
+        <main className="flex-1 p-2 sm:p-4 md:p-8">{children}</main>
       </div>
       <Footer />
     </div>
