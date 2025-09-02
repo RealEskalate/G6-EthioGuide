9071cb055e965e6f789199fa201595dff8183647
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2025-09-02 16:36:06 +0300

    Update route_names.dart add auth route name

--- ethioguide/lib/core/config/route_names.dart
+++ ethioguide/lib/core/config/route_names.dart
@@ -2,4 +2,5 @@ class RouteNames {
   static const String splash = 'splash';
   static const String onboarding = 'onboarding';
   static const String aiChat = 'Ai chat';
+  static const String auth = 'auth';
 }
