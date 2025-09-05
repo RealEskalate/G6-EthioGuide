9f67b8df7c8f22072e6dee6eac4c92b71b13bc3f
KidusHawoltu <kidushawoltu@gmail.com>
2025-09-05 10:02:27 +0300

    Update swagger host

--- delivery/main.go
+++ delivery/main.go
@@ -29,7 +29,7 @@ import (
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      https://ethio-guide-backend.onrender.com
+// @host      ethio-guide-backend.onrender.com
 // @BasePath  /api/v1
 
 // @securityDefinitions.apikey BearerAuth
