04e0d52d01217be830ec80ac622526106026e28b
sosena2 <149303452+sosena2@users.noreply.github.com>
2025-09-06 17:30:59 +0300

    feat:integrate api data to the notice page

--- app/store/store.ts
+++ app/store/store.ts
@@ -1,12 +1,13 @@
 // store/store.ts
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/workspaceSlice";
-import userReducer from './slices/userSlice';
+import userReducer from "./slices/userSlice";
 import { historyApi } from "./slices/historySlice";
-import { discussionsListApi } from "./slices/discussionsGetSlice"; // added
+import { discussionsListApi } from "./slices/discussionsGetSlice";
 // i18n bootstrap (prevents "NO_I18NEXT_INSTANCE" warnings app-wide)
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import { noticesApi } from "./slices/noticesSlice"; // added
 
 if (!i18n.isInitialized) {
   i18n.use(initReactI18next).init({
@@ -22,17 +23,18 @@ export const store = configureStore({
     user: userReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
     [historyApi.reducerPath]: historyApi.reducer,
-    [discussionsListApi.reducerPath]: discussionsListApi.reducer, // added
+    [discussionsListApi.reducerPath]: discussionsListApi.reducer,
+    [noticesApi.reducerPath]: noticesApi.reducer, // added
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       apiSlice.middleware,
       historyApi.middleware,
-      discussionsListApi.middleware // added
+      discussionsListApi.middleware,
+      noticesApi.middleware // added
     ),
   devTools: true,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
