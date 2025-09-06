ba6d4036fdf0bfda86a10ec9baab2b9a32adf3c2
@PhiliposHailu <148946615+PhiliposHailu@users.noreply.github.com>
2025-09-06 21:24:08 +0300

    Update providers.tsx

--- components/providers.tsx
+++ components/providers.tsx
@@ -18,10 +18,10 @@ export function Providers({ children }: ProvidersProps) {
     <SessionProvider>
       <I18nextProvider i18n={i18next}>
 
-<!--         <Provider store={store}> -->
-<!--           {children} -->
-<!--           <Toaster position="top-right" toastOptions={{ duration: 3000 }} /> -->
-<!--         </Provider> -->
+{/* <Provider store={store}> 
+{children} 
+<Toaster position="top-right" toastOptions={{ duration: 3000 }} /> 
+</Provider> */}
 
   <Provider store={store}>{children}<Toaster /></Provider>
       </I18nextProvider>
